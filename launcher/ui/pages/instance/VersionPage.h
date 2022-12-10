81f38108ca266fd087f7ffc62809b7221eba0349
TheKodeToad <TheKodeToad@proton.me>
2022-12-10 09:29:45 +0000

    Fix #562

--- launcher/ui/pages/instance/VersionPage.h
+++ launcher/ui/pages/instance/VersionPage.h
@@ -101,6 +101,8 @@ private:
     void updateButtons(int row = -1);
     void preselect(int row = 0);
     int doUpdate();
+    void fixModLoaderVersions(const QString& gameVersion);
+    void fixModLoaderVersion(const QString& id, const QString& gameVersion);
 
 protected:
     QMenu * createPopupMenu() override;
